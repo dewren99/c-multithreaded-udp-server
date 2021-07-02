50bab6c809ad132feed95a60cdd70d8c61b7eaaa
Deniz <devrendilek@gmail.com>
2021-07-01 23:56:25 -0700

    fixed bug char starting with ! terminated stalk

--- terminate.c
+++ terminate.c
@@ -4,7 +4,7 @@
 static const char TERMINATE[] = {'!'};
 
 void should_program_terminate(char *message) {
-    if (strncmp(TERMINATE, message, sizeof TERMINATE) == 0) {
+    if (strncmp(TERMINATE, message, sizeof message) == 0) {
         exit(0);
     }
 }
